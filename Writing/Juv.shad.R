5a6b1e1e82f4ddf7d974367bd3a8339f36112253
K-Hoffstats <87186928+K-Hoffstats@users.noreply.github.com>
2025-10-24 06:32:12 -0400

    Add files via upload

---
+++ Writing/Juv.shad.R
@@ -0,0 +1,30 @@
+##############################################################################
+#### Juvenile shad electrofishing survey ####
+#### last modified: 10232025
+
+# The purpose here is to recreate some of the traditional "looked at" graphics
+# and/or analyses for the juvenile shad report, for DFP-25. Intent is to make
+# things match the presentation quality of my other sections, now that no one
+# else is around to do this work...
+
+# In the past, they've presented as follows: 
+
+# Table 1. total sample trips, time, ams caught, and CPUE (#/min) separated out
+# for all the Santee + Congaree sites (A-G)--pretty straightforward here; this
+# should be an easy and informative thing to retain. "What sites produce...?"
+
+# Figure 3. bar chart of number of the "top 10" species caught--perhaps just 
+# make this a table instead; better visualization to easily compare years.
+
+# Figure 4.annual catch rates, plotted through the season (vs.DOY), with water
+# temp as the secondary y-axis
+
+# Figure 5. bar chart of # ams caught per year, fit with a line to represent total
+# effort (seconds)
+
+# Figure 6.stacked years, ams catch by month--this was actually adult ams, but if
+# we want to move forward with this (see monthly presence of the species) we should
+# just adapt this to my stacked graphic paradigm.
+
+
+
